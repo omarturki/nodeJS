96000fa8c3a00dd31190e6b556663f2260c5ea6c
omarturki <omarturkich@gmail.com>
2021-06-05 11:52:17 +0200

    the last version

--- exercice2.js
+++ exercice2.js
@@ -1,32 +1,25 @@
-
-getUser(1, (user) => {
-    getRepositories(user.gitHubUsername, (repos, levelNumber) => {
-        getBrunch(repos[levelNumber], (brunch) => {
-            if (brunch == "master")
-                postCommit(newVersion, (committed) => {
-                    if (committed)
-                        console.log("The new version is committed");
-                    else
-                        console.log("The new version is not committed");
-                })
-        })
-    })
-});
-
-/* 
-exercice 2 part 1*/
-
-getUser(1, (user) => {
-    getRepositories(user.gitHubUsername, (repos, levelNumber) => {
-        getBrunch(repos[levelNumber], brunch => postCommit(newVersion,
-            committed => console.log("the new version is" + committed)))
-    })
-});
-function getBrunch(r, callback1) {
-    if (brunch == "master")
-        callback1();
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Getting user ...  ');
+        callback({ id: id, gitHubUsername: 'omarturki' });
+    }, 2000)
 }
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback('repository');
+    }, 2000);
+}
+
+
+function getBrunch(repo, callback) {
+    setTimeout(() => {
+        callback('commit');
+    }, 2000)
+}
+
 function postCommit(n, callback) {
+    let committed = true; 
     if (committed) {
         let data = "commited";
         callback(data);
@@ -37,57 +30,107 @@ function postCommit(n, callback) {
 
 }
 
-/* exercice 2 part 2 */
-getUser(1, (user) => {
-    getRepositories(user.gitHubUsername, (repos, levelNumber) => {
-        return new Promise((resolve, reject) => {
-            resolve(getBrunch(repos[levelNumber], (brunch) => {
-                if (brunch == "master")
-                    postCommit(newVersion, committed => console.log("the new version is" + committed));
-            }))
-        })
+getusers = (id) => {
+    getUser(id, getRepository)
+}
 
-    })
-});
+getRepository= (b) => {
+    getRepositories(b, getBrunchs)
+}
 
-function postCommit(n, callback) {
+getBrunchs = (c) => {
+    getBrunch(c, postCommits)
+}
+
+postCommits = (d) => {
+    postCommit(d, console.log)
+}
+
+getusers(5)
+
+
+// exercice 2 part 2 (promise )
+getUser = (a) => {
     return new Promise((resolve, reject) => {
-        if (committed) {
-            resolve(callback(data));
-        } else {
-            let data = "not commited";
-            resolve(callback(data));
-        }
-    });
 
-
-}
-/* exercice 2 part 3 */
-
-async function getUser(n, user) {
-    let next = await getBrunch(repos[levelNumber], postCommit)
-    next = await callback()
-
-}
-getUser(1, (user) => {
-    getRepositories(user.gitHubUsername, (repos, levelNumber) => {
-        getBrunch(repos[levelNumber], brunch => postCommit(newVersion,
-            committed => console.log("the new version is" + committed)))
+        let user = 'omarturki';
+        if (user !== null)
+            resolve(user.gitHubUsername)
+        else
+            reject('error in  User')
     })
-});
-function getBrunch(r, callback1) {
-    if (brunch == "master")
-        callback1();
 }
-function postCommit(n, callback) {
-    if (committed) {
-        let data = "commited";
-        callback(data);
-    } else {
-        let data = "not commited";
-        callback(data);
+getRepositories = (b) => {
+
+    return new Promise((resolve, reject) => {
+        let repos =  "Dsnode";
+        let levelNumber = 0;
+        if (repos !== null && levelNumber !== null) {
+            resolve([repos, levelNumber])
+        } else {
+            reject("error levet function in  Repository !!!")
+        }
+
+    })
+}
+
+getBrunch = (c) => {
+    return new Promise((resolve, reject) => {
+
+        let brunch = "master"; 
+        console.log("get Brunch")
+        if (brunch !== null)
+            resolve(brunch)
+        else
+            reject("error in the brunch !")
+
+
+    })
+}
+
+postCommit = (d) => {
+
+    return new Promise((resolve, reject) => {
+        let committed = true
+        console.log("post Commit")
+        if (d === "master") {
+            if (committed)
+                resolve("The new version is Commited")
+            else
+                resolve("The new version is not commited")
+        } else {
+            reject("error not master")
+        }
+
+    })
+}
+
+
+getUser(1)
+    .then(getRepositories)
+    .then(getBrunch)
+    .then(postCommit)
+    .then(console.log)
+    .catch(console.log)
+
+
+
+/// exercice 2 part 3 async 
+
+getuser2 = async(a) => {
+    try {
+        let user = { 'gitHubUsername': "omarturki" } 
+
+        let [repos, levelNumber] = await getRepositories(user.gitHubUsername);
+
+        brunch = await getBrunch(repos[levelNumber]);
+        verif = await postCommit(brunch)
+        console.log(verif)
+    } catch (e) {
+        console.log(e)
     }
 
-}
-getUser(1, user);
 
+}
+
+getuser2(1)
